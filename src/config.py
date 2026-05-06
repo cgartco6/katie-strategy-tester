d5bbcca19acf482a58aae61459ff5ef273a25fa4
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-05-06 11:06:12 +0200

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,15 @@
+# Major Pairs for Testing
+MAJOR_PAIRS = {
+    "EURUSD": "EURUSD=X",
+    "GBPUSD": "GBPUSD=X",
+    "USDJPY": "JPY=X",
+    "AUDUSD": "AUDUSD=X",
+    "USDCHF": "CHF=X"
+}
+
+# Video Strategy Settings
+WPR_PERIOD = 7
+EMA_FAST = 11
+EMA_SLOW = 55
+PSAR_STEP = 0.02
+PSAR_MAX = 0.3
